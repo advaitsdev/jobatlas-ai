1e9699358ad077e3e3a602b31ef53e3abd79bfa9
Advait Sahadev <advaitsdev@gmail.com>
2026-07-07 23:58:56 +0530

    feat: initialize project structure and frontend foundation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { appName } from "@/lib/test";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+      <h1>{appName}</h1>
+    </div>
+  );
+}
+
+export default App;
